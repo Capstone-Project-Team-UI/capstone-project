b1cfab1b036dcf7df01fb95cc855fffd36eea403
Upal113 <upalkundu287@gmail.com>
2024-12-22 16:23:38 -0800

    Added Main Template

---
+++ src/main/java/com/capstone/cloudcontrol/remotepccontrol/RemotePcControlApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.cloudcontrol.remotepccontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemotePcControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemotePcControlApplication.class, args);
+	}
+
+}
